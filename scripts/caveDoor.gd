72a9959ce36c99a0ef3710615d3b9fe897f87442
andersAsch0 <ascho028@ucr.edu>
2023-07-06 12:02:05 -0700

    made example scenes for tree and grass, fixed dependencies

--- scripts/caveDoor.gd
+++ scripts/caveDoor.gd
@@ -1,16 +1,5 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# idk how this script got deleted thats very annoying
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
