5a1e666b2e2e67b6b1f3bfe879f69c774d88d49a
andersAsch0 <ascho028@ucr.edu>
2023-07-06 09:09:00 -0700

    bits and bobs

---
+++ Global.gd
@@ -0,0 +1,7 @@
+#this is a global script (created via project settings > autoload)
+#it is always running, and thus this variable is always accessible to other scripts
+
+extends Node
+
+var door_name = null
+
