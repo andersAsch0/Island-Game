6ef0bfdb0a95c026a1dc516536e3b3adf9f03908
andersAsch0 <ascho028@ucr.edu>
2023-07-19 18:05:00 -0700

    messing around w grid jumping
    this didnt rly work, new plan

--- scripts/battleMode/BattleModePlayer.gd
+++ scripts/battleMode/BattleModePlayer.gd
@@ -16,7 +16,10 @@ var walk_animations = ["horizontal", "horizontal", "vertical", "vertical"]
 var dash_animations = ["dash left", "dash right", "dash up", "dash down"]
 var direction = UP
 var velocity = Vector2.ZERO
+var storagePos = Vector2.ZERO
 
+func _ready():
+	storagePos = position
 
 func _process(delta):
 	
@@ -58,22 +61,45 @@ func _input(event):
 #		isDashing = true
 #		$DashTimer.start()
 	if(event.is_action_pressed("ui_up")):
-		position.y -= 20
+		updateInputTimer()
+		storagePos.y -= 20
 	elif(event.is_action_released("ui_up")):
-		position.y += 20
+		updateInputTimer()
+		storagePos.y += 20
 	elif(event.is_action_pressed("ui_down")):
-		position.y += 20
+		updateInputTimer()
+		storagePos.y += 20
 	elif(event.is_action_released("ui_down")):
-		position.y -= 20
+		updateInputTimer()
+		storagePos.y -= 20
 	elif(event.is_action_pressed("ui_right")):
-		position.x += 20
+		updateInputTimer()
+		storagePos.x += 20
 	elif(event.is_action_released("ui_right")):
-		position.x -= 20
+		updateInputTimer()
+		storagePos.x -= 20
 	elif(event.is_action_pressed("ui_left")):
-		position.x -= 20
+		updateInputTimer()
+		storagePos.x -= 20
 	elif(event.is_action_released("ui_left")):
-		position.x += 20		
+		updateInputTimer()
+		storagePos.x += 20		
 		
+# new plan: when button is pressed, start moving immediately
+# but the jump to another square takes a little time (animation w invuln)
+# so during that animation, it will note any more inputs and redirect you mid jump
+# (bur doesnt restart jump time, so jumps are always the same length to avoid most cheesing)
+		
+func updateInputTimer():
+	if $inputTimer.time_left == 0:
+		$inputTimer.start()
+	# on any of those 8 actions, start timer
+	# on timer end, update position
+	# actions during timer cant restart it
+func _on_inputTimer_timeout():
+	position = storagePos
 		
 func getHit(damage:int):
 	print("ouch")
+
+
