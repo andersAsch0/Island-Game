018a2df1a25e869aaa724d6baebe697111e00948
andersAsch0 <ascho028@ucr.edu>
2023-09-04 21:25:31 -0700

    decided how I want to rework things, started on new scene
    new music attack controller will be parent of the musicHandler, which will unique to each enemy and signals will need to be set up by battlemode.gd

---
+++ scripts/battleMode/musicAttackController.gd
@@ -0,0 +1,21 @@
+extends Node
+
+signal gridAttack(xPos, yPos)
+signal bulletAttack(pos)
+signal laserAttack(pos)
+
+
+func on_track_1(timeInAdvance):
+	pass
+
+func on_track_2(timeInAdvance):
+	pass
+	
+func on_track_3(timeInAdvance):
+	pass
+
+func on_track_4(timeInAdvance):
+	pass
+
+func on_track_5(timeInAdvance):
+	pass
